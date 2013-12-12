2e269bf20bf6ad693eaf9d07ee40630e60e00c95
Greedoid <jasieni2@illinois.edu>
2013-12-11 18:27:27 -0600

    Mastered the masteries

--- hercule.py
+++ hercule.py
@@ -1,24 +1,38 @@
+from config import *
 import requests
-import urllib2
-import sys
-
 
 class Request:
 
 	def __init__(self, api_key):
 		self.prior = 'http://prod.api.pvp.net/api/lol/'
 		self.vers = '/v1.1'
-		self.posterior = '?api_key=' + str(api_key)
+		self.api_key = str(api_key)
 
 	def get_id_from_name(self, name, region='na'):
-		request_string = self.prior + region + self.vers + '/summoner/by-name/' + name + self.posterior
+		request_string = self.prior + region + self.vers + '/summoner/by-name/' + name 
 		return self.make_request(request_string)['id']
 
 	def get_level_from_name(self, name, region='na'):
-		request_string = self.prior + region + self.vers + '/summoner/by-name/' + name + self.posterior
+		request_string = self.prior + region + self.vers + '/summoner/by-name/' + name 
 		return self.make_request(request_string)['summonerLevel']
 
-	def make_request(self, request_string):
-		req = urllib2.Request(request_string)
-		response = urllib2.urlopen(req)
-		return eval(response.read())
+	def get_masteries_from_name(self, name, region='na'):
+		request_string = self.prior + region + self.vers + '/summoner/' + str(self.get_id_from_name(name)) + '/masteries' 
+		raw = self.make_request(request_string)
+		pages = []
+		for item in raw['pages']:
+			pages.append(item)
+		return pages
+
+	def get_masteries_from_id(self, summoner_id, region='na'):
+		request_string = self.prior + region + self.vers + '/summoner/' + summoner_id + '/masteries' 
+		raw = self.make_request(request_string)
+		pages = []
+		for item in raw['pages']:
+			pages.append(item)
+		return pages
+
+	def make_request(self, request_info):
+		r = requests.get(request_info, params={'api_key': self.api_key})
+		return r.json()
+
